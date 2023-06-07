f0d9d42dc54239093b878c9ebc75897d7346739c
gaguriee <74501631+gaguriee@users.noreply.github.com>
2023-06-07 22:36:13 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 292 ms, Memory: 20988 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int[] seq;
+    static int[] dp;
+    static int num;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        num = scanner.nextInt();
+        seq = new int[num];
+        dp = new int[num]; // cost
+
+        for (int i = 0; i < num; i++) {
+            seq[i] = scanner.nextInt();
+        }
+
+        LIS();
+
+        int answer = 0;
+        for (int i = 0; i < num; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+
+    }
+
+    public static void LIS() {
+        for (int i = 0; i < num; i++) {
+
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (seq[j] < seq[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+
+        }
+    }
+
+}
