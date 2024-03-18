39887be07667bc1a16949a3751f85c83f5e57c4b
gaguriee <74501631+gaguriee@users.noreply.github.com>
2024-03-18 23:34:08 +0900

    [Silver I] Title: 절댓값 힙, Time: 148 ms, Memory: 39824 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -2,16 +2,16 @@
 
 import sys, heapq
 
-N = int(sys.stdin.readline())
+N = int(sys.stdin.readline()) # 시간초과 방지
 heap = []
 
 for i in range(N):
     x = int(sys.stdin.readline())
     if x == 0:
         if heap:
-            print(heapq.heappop(heap)[1])
+            print(heapq.heappop(heap)[1]) # 실제 값만 출력
         else:
             print(0)
 
     else:
-        heapq.heappush(heap, [abs(x), x]) 
+        heapq.heappush(heap, (abs(x), x)) # 튜플로 push 
