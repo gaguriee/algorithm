936c5dad6795342a49b717829d41663d8d12b3f2
gaguriee <74501631+gaguriee@users.noreply.github.com>
2024-03-21 00:20:31 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -0,0 +1,32 @@
+import sys
+
+c = int(sys.stdin.readline())
+answer = []
+
+for _ in range(c):
+
+    N=int(sys.stdin.readline())
+    tree= [0] * (N+1)
+ 
+    for _ in range(N-1):
+        a,b=map(int, sys.stdin.readline().split())
+        tree[b] = a
+
+    x,y=map(int, sys.stdin.readline().split()) # 공통조상 찾을 두 노드
+    
+    # x의 조상 집합 찾기
+    x_parent = [] 
+    while x:
+        x_parent.append(x)
+        x=tree[x]
+
+    # x와 y의 공통 조상 찾기
+    y_parent = [] 
+    while y:
+        if y in x_parent:
+            answer.append(y)
+            break
+        y=tree[y]
+
+for ans in answer:
+    print(ans)
