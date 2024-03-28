efd49b441f3ad03701c7e91ceb66d5dcbc4ad3fe
gaguriee <74501631+gaguriee@users.noreply.github.com>
2024-03-29 01:22:46 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 4208 ms, Memory: 47652 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,46 @@
+# 1197 최소 스패닝 트리
+
+# 크루스칼 알고리즘을 위한 부모 찾기 함수
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 정점의 개수 V와 간선의 개수 E 입력 받기
+V, E = map(int, input().split())
+parent = [0] * (V + 1)  # 부모 테이블 초기화
+
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = 0
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, V + 1):
+    parent[i] = i
+
+# 모든 간선에 대한 정보를 입력 받기
+for _ in range(E):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+# 간선을 비용순으로 정렬
+edges.sort()
+
+# 간선을 하나씩 확인하며
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
